47a94c3295cadfd9fce72f6d4cf4fd63ba08a279
isrt09 <mazedur@gmail.com>
2021-02-03 03:22:55 +0600

    custom services template

---
+++ page-services.php
@@ -0,0 +1,33 @@
+<?php 
+/*
+Template Name: Service Template
+*/
+get_header(); ?>
+<!-- ::::::::::::::::::::: Page Title Section:::::::::::::::::::::::::: -->
+    <?php while(have_posts()) : the_post(); ?>
+		<section <?php if(has_post_thumbnail()) : ?> style="background-image: url(<?php the_post_thumbnail_url('large'); ?>)" <?php endif; ?> class="page-title">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12">
+						<!-- breadcrumb content -->
+						<div class="page-breadcrumbd">
+							<h2><?php the_title(); ?></h2>
+							<p><a href="<?php echo site_url(); ?>">Home</a> / <?php the_title(); ?></p>
+						</div><!-- end breadcrumb content -->
+					</div>
+				</div>
+			</div>
+		</section><!-- end breadcrumb -->				
+		<!-- ::::::::::::::::::::: Block Section:::::::::::::::::::::::::: -->
+		<?php get_template_part('common/content','services'); ?>   
+		<section class="block about-us-block ">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12">															
+						<?php the_content(); ?>						
+					</div>					
+				</div>
+			</div>
+		</section><!-- block area end -->		
+	<?php endwhile; ?>
+<?php get_footer(); ?>
